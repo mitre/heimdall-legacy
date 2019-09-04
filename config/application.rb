32e939bd2aa4ab451e88f4a0d9c47f97e0bf6c62
Robert Thew <rthew@mitre.org>
2019-09-04 14:08:51 -0400

    Should work with a relative path

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module HeimdallPostgres
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.assets.prefix = "#{ENV['HEIMDALL_RELATIVE_URL_ROOT']}#{config.assets.prefix}"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
