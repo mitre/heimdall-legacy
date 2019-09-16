22ec2d351121a96c5ef42bafc776d2292f9b777d
Robert Thew <rthew@mitre.org>
2019-09-16 15:06:29 -0400

    updated cookie settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -28,6 +28,8 @@ Devise.setup do |config|
   config.ldap_use_admin_to_bind = true
   # config.ldap_ad_group_check = false
 
+  config.rememberable_options = { same_site: :lax }
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
