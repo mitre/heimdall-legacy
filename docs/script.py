1e87e33ed5dbefaddc1ac8b382f57a239ecb64b9
Mark Tollick <redsoxfn04@gmail.com>
2019-10-21 00:06:26 -0400

    updated logic in the if/elif/else statement. now properly detects user input. put current path back in.

--- docs/script.py
+++ docs/script.py
@@ -18,11 +18,13 @@ with open(nameOfFile) as csvDataFile:
         hostNames.append(row[0])
         ipAddresses.append(row[1])
 
-if scanType == "host name" or "hostname" or "Host name" or "Hostname":
+if (scanType == "host name" or scanType =="hostname" or scanType =="Host name" or scanType =="Hostname"):
     for hostName in hostNames:    
         inspecScan = "sudo inspec exec inspec-profile-disa_stig-el7/ --attrs=inspec-profile-disa_stig-el7/attributes.yml -t ssh://" + username + "@" + hostName + " --sudo --sudo-password=" + password + " --password=" + password + " --reporter cli json:" + currentPath + hostName + "-" + str(today) + ".json"
         os.system(inspecScan)
-elif scanType == "ip address" or "ipaddress" or "Ip Address" or "IP Address" or "IP address":
+elif (scanType == "ip address" or scanType =="ipaddress" or scanType =="Ip Address" or scanType =="IP Address" or scanType =="IP address"):
     for ipAddress in ipAddresses:
         inspecScan = "sudo inspec exec inspec-profile-disa_stig-el7/ --attrs=inspec-profile-disa_stig-el7/attributes.yml -t ssh://" + username + "@" + ipAddress + " --sudo --sudo-password=" + password + " --password=" + password + " --reporter cli json:" + currentPath + ipAddress + "-" + str(today) + ".json"
         os.system(inspecScan)
+else:
+    scanType = input("How would you like to run this scan, via host name or via ip address?")
