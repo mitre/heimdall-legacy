5d7838f8b9b24e8c6d3d142711cbe1e5f18dfb89
Mark Tollick <redsoxfn04@gmail.com>
2019-10-31 15:33:45 -0400

    updated powershell script to include username/password pull in, still working on encrypition.
    Signed-off-by: Mark Tollick <redsoxfn04@gmail.com>

--- docs/automatedScan.ps1
+++ docs/automatedScan.ps1
@@ -2,20 +2,26 @@ $basePath=Get-Location
 $pathEnd="ip.csv"
 $path= Join-Path -Path $basePath $pathEnd
 $folderName=(Get-Date -Format "MM-dd-yyy_HH-mm-ss")
-$csv=Import-Csv -Path $path -Header hostname,IPAddress
+$hostCSV=Import-Csv -Path $path -Header hostname,IPAddress
+$userCredentials=(Get-Content "$basePath\userCredentials.csv").split(",")
+$Credential = Get-Credential
+Write-Host $Credential
+
+
+
 New-Item -Path "$basePath\$folderName" -ItemType "directory"
 
-
-
-function inspecScan([String]$hostName){
+function inspecScan([String]$hostName, [String]$ipAddress,[String]$username,[String]$password){
     Start-Process "cmd.exe" "/c inspec exec "
 }
 
-foreach ($line in $csv) {
-    foreach ($item in $line){
-        $hostName=@($item)
-        Write-Host $hostName
-    }
+foreach ($item in $hostCSV) {
+    $hostName=$($item.hostname)
+    $ipAddress=$($item.IPAddress)
 }
+
+
+
+
 
 
