1aae26c0feaa58370e3c3a2be456b0a4be6ba429
Mark Tollick <redsoxfn04@gmail.com>
2019-11-01 12:33:42 -0400

    Modified script to include handling of encrypted user credentials.
    Signed-off-by: Mark Tollick <redsoxfn04@gmail.com>

--- docs/automatedScan.ps1
+++ docs/automatedScan.ps1
@@ -3,16 +3,16 @@ $pathEnd="ip.csv"
 $path= Join-Path -Path $basePath $pathEnd
 $folderName=(Get-Date -Format "MM-dd-yyy_HH-mm-ss")
 $hostCSV=Import-Csv -Path $path -Header hostname,IPAddress
-$userCredentials=(Get-Content "$basePath\userCredentials.csv").split(",")
-$Credential = Get-Credential
-Write-Host $Credential
+$username=Get-Content "$basePath\username.txt" | ConvertTo-SecureString
+$password=Get-Content "$basePath\password.txt" | ConvertTo-SecureString
+
 
 
 
 New-Item -Path "$basePath\$folderName" -ItemType "directory"
 
 function inspecScan([String]$hostName, [String]$ipAddress,[String]$username,[String]$password){
-    Start-Process "cmd.exe" "/c inspec exec "
+    Start-Process "cmd.exe" "/c inspec exec stig-microsoft-windows-server-2016-v1r14-baseline -t winrm://$usersname@$hostName --password=$password --reporter cli json:$hostname.json"
 }
 
 foreach ($item in $hostCSV) {
