adb9d4f00ad5579a780a778d30f98483f030ae22
mrtollick <redsoxfn04@gmail.com>
2019-11-21 13:12:34 -0500

    Updated documentation to include pushing to the heimdall server in the automatedScan script, as well as the cookie cutter command to edit to push.
    Signed-off-by: mrtollick <redsoxfn04@gmail.com>

--- docs/automatedScan.sh
+++ docs/automatedScan.sh
@@ -14,3 +14,10 @@ sudo inspec exec [/path/to/windows/profile/you/want/to/run]/stig-microsoft-windo
 sudo inspec exec [/path/to/linux/profile/you/want/to/run]/inspec-profile-disa_stig-el7/ --attrs=[/path/to/linux/profile/you/want/to/run]/inspec-profile-disa_stig-el7/attributes.yml -t ssh://$usernam@[hostname or ip address] --sudo --sudo-password=$password --password=$password --reporter cli json:[/path/to/directory/where/scans/are/saved/hostname or ipaddress]-$today.json
 
 # repeat command for specific linux hosts 
+
+
+# Here is the command to push to the heimdall API. You must repeat for every file you wish to upload, which is the number of scans you are doing on your machine. Change the values in the [ and ]
+# Brackets. 
+
+
+curl -F "file=@[FILE_PATH]" -F "email=[EMAIL]" -F "api_key=[API_KEY]" http://localhost:3000/evaluation_upload_api
