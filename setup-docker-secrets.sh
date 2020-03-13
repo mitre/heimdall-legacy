fb76acf031201f279699610bfbe24bc7a1e7133f
Jarod Neuner <jneuner@mitre.org>
2020-03-13 20:35:28 +0000

    Added handling for the postgresql password during docker setup.
    Signed-off-by: Jarod Neuner <jneuner@mitre.org>

--- setup-docker-secrets.sh
+++ setup-docker-secrets.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ -f .env ]; then
+	echo ".env already exists, if you would like to regenerate your secrets, please delete this file and re-run the script."
+else
+	echo ".env does not exist, creating..."
+	(umask 066; touch .env)
+fi
+
+if ! grep -qF "POSTGRES_PASSWORD" .env; then
+	echo ".env does not contain POSTGRES_PASSWORD, generating secret..."
+	echo -e "\nPOSTGRES_PASSWORD=$(openssl rand -base64 33)" >> .env
+fi
+
+
 if [ -f .env-prod ]; then
 	echo ".env-prod already exists, if you would like to regenerate your secrets, please delete this file and re-run the script."
 else
@@ -10,4 +23,5 @@ CIPHER_PASSWORD=$(openssl rand -hex 64)
 CIPHER_SALT=$(openssl rand -hex 32)
 EOF
 fi
+
 echo "Done"
